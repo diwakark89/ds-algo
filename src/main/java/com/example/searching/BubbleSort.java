3d896e7c75eda16b98fdec55ca7fa995ff5e49f9
Diwakar Kushwaha <diwakar.nitk@gmail.com>
2021-05-15 09:50:03 +0530

    Initial setup

---
+++ src/main/java/com/example/searching/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.example.searching;
+
+public class BubbleSort {
+
+    public void sort(int arr[]){
+
+        boolean swap=true;
+        int itr=1;
+        for(int i=arr.length-1;i>=0&&swap;i-- ){
+            swap=false;
+            for (int j=0;j<i;j++){
+                if(arr[j]>arr[j+1]){
+                    swap=true;
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+                itr++;
+            }
+
+        }
+        System.out.println(""+itr);
+    }
+}
