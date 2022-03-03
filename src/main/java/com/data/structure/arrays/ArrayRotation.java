0cab28baf5b814e6ef7609346b55751df2942474
Diwakar Kushwaha <diwakar.nitk@gmail.com>
2022-03-03 08:23:10 +0530

    Binary Search algorithm

--- src/main/java/com/data/structure/arrays/ArrayRotation.java
+++ src/main/java/com/data/structure/arrays/ArrayRotation.java
@@ -1,12 +1,45 @@
 package com.data.structure.arrays;
 
+/**
+ * Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
+ * */
 public class ArrayRotation {
-    public static void main(String []args){
-        ArrayRotation ar = new ArrayRotation();
-        System.out.println(ar.gcd(2, 7));
+    public static void main(String[] args) {
+        ArrayRotation rotate = new ArrayRotation();
+        int arr[] = {1, 2, 3, 4, 5, 6, 7};
+        rotate.leftRotate(arr, 3, 7);
+        rotate.printArray(arr, 7);
     }
-    int gcd(int a, int b)
-    {
+
+    void printArray(int arr[], int size) {
+        for (int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+    }
+
+    void leftRotate(int arr[], int d, int n) {
+        d = d % n;
+        int i, j, k, temp;
+        int g_c_d = gcd(d, n);
+
+        for (i = 0; i < g_c_d; i++) {
+            temp = arr[i];
+            j = i;
+            while (true) {
+                k = j + d;
+                if (k >= n) {
+                    k = k - n;
+                }
+                if (k == i) {
+                    break;
+                }
+                arr[j] = arr[k];
+                j = k;
+            }
+            arr[j] = temp;
+        }
+    }
+
+    int gcd(int a, int b) {
         if (b == 0)
             return a;
         else
