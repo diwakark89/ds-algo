9cfb00d53708fe398b777328cd5626dcb56e7d16
Diwakar Kushwaha <diwakark.nitk@gmail.com>
2026-02-25 07:22:14 +0100

    Remove unused code
    Signed-off-by: Diwakar Kushwaha <diwakark.nitk@gmail.com>

---
+++ README.md
@@ -0,0 +1,6 @@
+# ds-algo
+
+A small Java workspace with data structures and algorithm practice code.
+Includes sample problems, test runners, and reference implementations.
+Built with Maven and organized under src/main/java.
+
--- src/main/java/com/data/structure/Print.java
+++
@@ -1,9 +0,0 @@
-package com.data.structure;
-
-public class Print {
-    public static void printArray(int[] arr, int size) {
-        for (int i = 0; i < size; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-}
